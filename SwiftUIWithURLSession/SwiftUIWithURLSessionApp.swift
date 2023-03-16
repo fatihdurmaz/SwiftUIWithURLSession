7712a72864ae54383e3ad92f281e73135db243a2
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-03-16 22:39:53 +0300

    Initial Commit

---
+++ SwiftUIWithURLSession/SwiftUIWithURLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithURLSessionApp.swift
+//  SwiftUIWithURLSession
+//
+//  Created by Fatih Durmaz on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
